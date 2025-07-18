3edee253349e53a58a2356100bee7549ec636865
pavel <pavelzubar@gmail.com>
2025-07-18 13:48:25 +0300

    Initial commit

---
+++ src/main/java/lesson_15/AutomatedTestCases.java
@@ -0,0 +1,24 @@
+package lesson_15;
+
+public class AutomatedTestCases {
+
+
+    public static class RegistrationModule {
+
+        /**
+         * Title: User is able to register using valid data
+         * Steps:
+         * 1. Go to https://qa-course-01.andersenlab.com/registration
+         * 2. Enter first name
+         * 3. Enter last name
+         * 4. Enter date of birth
+         * 5. Enter email
+         * 6. Enter password
+         * 7. Enter confirm password the same value as password
+         * 8. Click "Submit" button
+         */
+        public static boolean registerWithValidData() {
+
+        }
+    }
+}
---
+++ src/main/java/lesson_15/HomeworkExecution.java
@@ -0,0 +1,9 @@
+package lesson_15;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HomeworkExecution {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+    }
+}
