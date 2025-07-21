f3f7c8313257d7987edb0f4d9e38c3ce41f8f190
pavel <pavelzubar@gmail.com>
2025-07-21 17:46:58 +0300

    Added code to priority task, added groups task

---
+++ src/test/java/lesson_16/grouping/Groups.java
@@ -0,0 +1,47 @@
+package lesson_16.grouping;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Groups {
+
+    @Test
+    public void one() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void two() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void three() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void four() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void five() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void six() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void seven() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void eight() {
+        Assert.assertTrue(true);
+    }
+}
--- src/test/java/lesson_16/priority/Priority1.java
+++ src/test/java/lesson_16/priority/Priority1.java
@@ -4,24 +4,39 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Priority1 {
-    @Test
-    public void c() { Assert.assertTrue(true); }
 
-    @Test
-    public void e() { Assert.assertTrue(true); }
+    @Test(priority = 5, dependsOnMethods = "d", alwaysRun = true)
+    public void c() {
+        Assert.assertTrue(true);
+    }
 
-    @Test
-    public void a() { Assert.assertTrue(true); }
+    @Test(priority = 3, dependsOnMethods = "f", alwaysRun = true)
+    public void e() {
+        Assert.assertTrue(true);
+    }
 
-    @Test
-    public void g() { Assert.assertTrue(true); }
+    @Test(priority = 7, dependsOnMethods = "b", alwaysRun = true)
+    public void a() {
+        Assert.assertTrue(true);
+    }
 
-    @Test
-    public void b() { Assert.assertTrue(true); }
+    @Test(priority = 1)
+    public void g() {
+        Assert.assertTrue(true);
+    }
 
-    @Test
-    public void d() { Assert.assertTrue(true); }
+    @Test(priority = 6, dependsOnMethods = "c", alwaysRun = true)
+    public void b() {
+        Assert.assertTrue(true);
+    }
 
-    @Test
-    public void f() { Assert.assertTrue(true); }
+    @Test(priority = 4, dependsOnMethods = "e", alwaysRun = true)
+    public void d() {
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 2, dependsOnMethods = "g", alwaysRun = true)
+    public void f() {
+        Assert.assertTrue(true);
+    }
 }
