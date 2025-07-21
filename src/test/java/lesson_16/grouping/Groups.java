2e19258b241bd788d99296632b460e029c19389d
pavel <pavelzubar@gmail.com>
2025-07-21 17:51:16 +0300

    Added groups, added groups xml

--- src/test/java/lesson_16/grouping/Groups.java
+++ src/test/java/lesson_16/grouping/Groups.java
@@ -5,42 +5,42 @@ import org.testng.annotations.Test;
 
 public class Groups {
 
-    @Test
+    @Test(groups = {"first"}, priority = 1)
     public void one() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"second"}, priority = 1)
     public void two() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"first"}, priority = 2)
     public void three() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"second"}, priority = 2)
     public void four() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"first"}, priority = 3)
     public void five() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"second"}, priority = 3)
     public void six() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"first"}, priority = 4)
     public void seven() {
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(groups = {"second"}, priority = 4)
     public void eight() {
         Assert.assertTrue(true);
     }
