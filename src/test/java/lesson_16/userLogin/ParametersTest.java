c205b1a194a9d570ec0241ec264f57b33de74f52
pavel <pavelzubar@gmail.com>
2025-07-21 20:23:21 +0300

    Added tests of login and xml file with parameters

---
+++ src/test/java/lesson_16/userLogin/ParametersTest.java
@@ -0,0 +1,50 @@
+package lesson_16.userLogin;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class ParametersTest {
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    @Parameters({"email", "password"})
+    public void testingTest(String email, String password) {
+
+        driver.get("https://qa-course-01.andersenlab.com/login");
+        driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys(password);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type=\"submit\"]"))).click();
+
+        wait.until(ExpectedConditions.urlToBe("https://qa-course-01.andersenlab.com/"));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa-course-01.andersenlab.com/", "Wrong page URL!");
+        By emailLocator = By.xpath("//div[text()=\"" + email + "\"]");
+        Assert.assertFalse(driver.findElements(emailLocator).isEmpty(), "Could not confirm the correct user login!");
+
+    }
+
+    @AfterClass
+    public void quitDriver() {
+        driver.quit();
+    }
+
+}
