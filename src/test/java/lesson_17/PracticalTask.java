9b2640a7d53d22a2c16969bdadfdba0f2da52d5f
pavel <pavelzubar@gmail.com>
2025-07-24 16:30:45 +0300

    Added base

--- src/test/java/lesson_17/PracticalTask.java
+++ src/test/java/lesson_17/PracticalTask.java
@@ -2,6 +2,7 @@ package lesson_17;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,6 +17,8 @@ public class PracticalTask {
     //aqastud@mail.com
     //Qwer123$
 
+    public WebDriver driver;
+
     @BeforeClass
     public void setupDriver() {
         WebDriverManager.chromedriver().setup();
@@ -27,8 +30,13 @@ public class PracticalTask {
         driver.quit();
     }
 
+    public static void selectMenuItem(String){
+        driver.findElement(By.xpath("//div[text()='AQA Practice']"));
+    }
+
     @Test
     public void selectChoosing() {
         //div[text()='AQA Practice']
+        //div[text()='Select']
     }
 }
