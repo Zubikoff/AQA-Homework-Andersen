ee9f76acef60d5c1c27bf20be4e79afe2127a248
pavel <pavelzubar@gmail.com>
2025-07-24 16:28:07 +0300

    Added base

--- src/test/java/lesson_17/PracticalTask.java
+++ src/test/java/lesson_17/PracticalTask.java
@@ -1,10 +1,31 @@
 package lesson_17;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class PracticalTask {
     //aqastud@mail.com
     //Qwer123$
+
+    @BeforeClass
+    public void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void quitDriver() {
+        driver.quit();
+    }
 
     @Test
     public void selectChoosing() {
