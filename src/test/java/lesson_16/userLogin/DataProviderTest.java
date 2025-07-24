f4a263f75ab978b67471a17cd809e77b6a0b8d29
pavel <pavelzubar@gmail.com>
2025-07-24 13:25:28 +0300

    Final checks

--- src/test/java/lesson_16/userLogin/DataProviderTest.java
+++ src/test/java/lesson_16/userLogin/DataProviderTest.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
