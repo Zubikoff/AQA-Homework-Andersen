f78b45906acc569982259a5abc50f5a861a4de2f
pavel <pavelzubar@gmail.com>
2025-07-24 16:26:45 +0300

    Added base

---
+++ src/test/java/lesson_17/PracticalTask.java
@@ -0,0 +1,13 @@
+package lesson_17;
+
+import org.testng.annotations.Test;
+
+public class PracticalTask {
+    //aqastud@mail.com
+    //Qwer123$
+
+    @Test
+    public void selectChoosing() {
+        //div[text()='AQA Practice']
+    }
+}
