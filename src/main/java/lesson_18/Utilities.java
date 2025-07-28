ff9b7df205b19fd68d7a6461b6e143fc1d97509e
pavel <pavelzubar@gmail.com>
2025-07-28 17:39:41 +0300

    Added LoginPage page object and RegistrationPage draft

---
+++ src/main/java/lesson_18/Utilities.java
@@ -0,0 +1,29 @@
+package lesson_18;
+
+import java.util.Random;
+
+public class Utilities {
+    /**
+     * @return Randomly generated email
+     */
+    public static String getValidEmail(){
+        return getRandomEmail();
+    }
+
+    /**
+     * @return Randomly generated email that is NOT equals to specified email
+     */
+    public static String getInvalidEmail(String email) {
+        String invalidEmail = getRandomEmail();
+        while (invalidEmail.equals(email)) {
+            invalidEmail = getRandomEmail(); // just in case randomizer returned the same email, reroll until it isn't
+        }
+        return invalidEmail;
+    }
+
+    private static String getRandomEmail(){
+        Random rand = new Random();
+        return "aqastud" + (rand.nextInt(9000)+999)
+                + "@mail" + (rand.nextInt(9000)+999) + ".com";
+    }
+}
---
+++ src/main/java/lesson_18/pages/RegistrationPage.java
@@ -0,0 +1,80 @@
+package lesson_18.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class RegistrationPage {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    private static final String pageURL = "https://qa-course-01.andersenlab.com/registration";
+    private static String email;
+
+    @FindBy(xpath = "\"//input[@name=\\\"email\\\"]\"")
+    private static WebElement emailField;
+
+    @FindBy(xpath = "//input[@name=\"password\"][@placeholder=\"Enter password\"]")
+    private static WebElement passwordField;
+
+    @FindBy(xpath = "//button[@type=\"submit\"]")
+    private static WebElement signInButton;
+
+    public RegistrationPage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        PageFactory.initElements(driver, this);
+    }
+
+    public RegistrationPage openLoginPage(){
+        driver.get(pageURL);
+        return this;
+    }
+
+    public RegistrationPage sendKeys(WebElement element, String text){
+        wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(text);
+        return this;
+    }
+
+    public RegistrationPage setEmail(String text){
+        sendKeys(emailField, text);
+        email = text;
+        return this;
+    }
+
+    public RegistrationPage setPassword(String text){
+        sendKeys(passwordField, text);
+        return this;
+    }
+
+    public RegistrationPage clickOnSignInButton(){
+        wait.until(ExpectedConditions.visibilityOf(signInButton)).click();
+        return this;
+    }
+
+    public boolean verifyExpectedTextOnPage(String text){
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(), '"
+                    + text + "')]")));
+        } catch (TimeoutException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean verifySignInButtonIsDisabled(){
+        try {
+            return wait.until(ExpectedConditions.visibilityOf(signInButton)).isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+}
